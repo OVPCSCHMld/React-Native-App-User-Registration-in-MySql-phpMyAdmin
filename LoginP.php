096f032b169a8022a3d36e498d3f5804d6fb1048
OVPCSCHMld <78312248+OVPCSCHMld@users.noreply.github.com>
2022-03-10 22:47:26 +0000

    Add files via upload

---
+++ LoginP.php
@@ -0,0 +1,30 @@
+<?php
+include('db.php');
+
+$Email = $Decode_React_APP_Data['Email'];
+$Password = $Decode_React_APP_Data['Password']; 
+
+
+$query = "SELECT * FROM registrationapp_react WHERE Email = '$Email'";
+$query_result = mysqli_query($connect_db, $query);
+
+
+if (mysqli_num_rows($query_result)) {
+$DB_Elements= mysqli_fetch_array($query_result);
+
+    if ($DB_Elements['Password'] == $Password) {
+        $Message = "Logged In Successfully";
+    } else { 
+		$Message = "Password is wrong";
+    }
+} 
+else 
+{
+    $Message = "User does not exist";
+}
+
+$response[] = array("Message" => $Message);
+
+echo json_encode($response);
+
+?>
---
+++ SignUp.php
@@ -0,0 +1,34 @@
+<?php
+include('db.php');
+
+$First_Name = $Decode_React_APP_Data['First_Name'];
+$Last_Name = $Decode_React_APP_Data['Last_Name'];
+$Email = $Decode_React_APP_Data['Email'];
+$Password = ($Decode_React_APP_Data['Password']); //password is un-hashed mean with no hacking capability(change to md5 for secure pass)
+
+
+$query = "SELECT * FROM registrationapp_react WHERE Email = '$Email'";
+$query_result = mysqli_query($connect_db, $query);
+
+if (!mysqli_num_rows($query_result))
+{
+    $Reg_Query = "INSERT INTO registrationapp_react(`First_Name`, `Last_Name`, `Email`, `Password`) VALUES ('$First_Name', '$Last_Name', '$Email', '$Password')";
+    $Reg_Query_Result = mysqli_query($connect_db, $Reg_Query);
+
+    if ($Reg_Query_Result) 
+	{
+        $Message = "Registered successfuly!";
+    } else 
+	{
+        $Message = "Error - Try again";
+    }
+	
+} else 
+{
+    $Message = "User Already Registered";
+}
+
+$response[] = array("Message" => $Message);
+
+echo json_encode($response);
+?>
