7374ffc6412886b4c29e821eb7631d3d539be2a9
OVPCSCHMld <78312248+OVPCSCHMld@users.noreply.github.com>
2022-11-18 00:30:05 +0000

    Delete Logout.php

--- Logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-    session_start();
-	session_destroy(); 
-	header("Location: LoginP.php");
-	$Message = "Logged Out";
-	$response[] = array("Message" => $Message);
-	echo json_encode($response);
-?>
